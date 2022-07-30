2f422a9d66cd393c05121da503473d82e166da50
Konstantinos Chatzispyrou <57675530+KostasChatz99@users.noreply.github.com>
2022-07-30 12:16:36 +0300

    Add files via upload

--- Askisi2.php
+++ Askisi2.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
+<?php
 
-<header>
-        <h1>
-            <?= echo "Hello, " . htmlspecialchars($GET['name']); ?>
-        </h1>
-</header>
+$greeting = "Hello, World";
 
-</body>
-</html>
+require 'Askisi2.view.php';
